32f6dd307c4d6a9540570604d47b5b089ccb246e
shuai <1247479290@qq.com>
2021-01-02 16:13:24 +0800

    修改git提交到本地

--- ideaproject20210102/src/main/java/com/broit/gitdemo0102/gitdemo01.java
+++ ideaproject20210102/src/main/java/com/broit/gitdemo0102/gitdemo01.java
@@ -1,11 +1,12 @@
 package com.broit.gitdemo0102;
 
 public class gitdemo01 {
+    //测试git啊啊
     public static void main(String[] args) {
         System.out.println("hahahahhahahhaahha");
         System.out.println("hahahahhahahhaahha");
         System.out.println("hahahahhahahhaahha");
-        System.out.println("hahahahhahahhaahha");
+        System.out.println("hahahahhahahhaahha1111111");
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
