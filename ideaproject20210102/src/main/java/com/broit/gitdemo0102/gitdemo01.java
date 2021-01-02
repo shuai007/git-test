10c7f48a149c803d659f6318307fc7be8e1f25c8
shuai <1247479290@qq.com>
2021-01-02 15:41:58 +0800

    第一次git提交到本地

---
+++ ideaproject20210102/src/main/java/com/broit/gitdemo0102/gitdemo01.java
@@ -0,0 +1,10 @@
+package com.broit.gitdemo0102;
+
+public class gitdemo01 {
+    public static void main(String[] args) {
+        System.out.println("hahahahhahahhaahha");
+        System.out.println("hahahahhahahhaahha");
+        System.out.println("hahahahhahahhaahha");
+        System.out.println("hahahahhahahhaahha");
+    }
+}
